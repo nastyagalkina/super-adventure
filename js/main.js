a36cdfaef486d43501018c308de3464dc1ed91c8
nastyagalkina <55530369+nastyagalkina@users.noreply.github.com>
2019-10-08 11:38:55 +0300

    08.10.19 таймер до конца дня

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ $(document).ready(function() {
       }
     });
   });
+
+  function tick () {
+    let curTime = new Date();
+    let hours = 23 - curTime.getHours();
+    if (hours < 10)
+      hours = "0" + hours;
+    let minutes = 59 - curTime.getMinutes();
+    if (minutes < 10)
+      minutes = "0" + minutes;
+    let seconds = 59 - curTime.getSeconds();
+    if (seconds < 10)
+      seconds = "0" + seconds;
+    document.getElementById("h").innerHTML = hours + " : ";
+    document.getElementById("m").innerHTML = minutes + " : ";
+    document.getElementById("s").innerHTML = seconds;
+  }
+  setInterval(tick,1000);
+  
